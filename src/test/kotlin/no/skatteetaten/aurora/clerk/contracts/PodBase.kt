2c4e49c064ed3f0f3e83be722c1a3de7ef9661bb
Jarle Hansen <jarle.hansen@skatteetaten.no>
2019-03-01 13:21:59 +0100

    fixed pod contract test

--- src/test/kotlin/no/skatteetaten/aurora/clerk/contracts/PodBase.kt
+++ src/test/kotlin/no/skatteetaten/aurora/clerk/contracts/PodBase.kt
@@ -1,28 +1,39 @@
 package no.skatteetaten.aurora.clerk.contracts
 
 import com.nhaarman.mockito_kotlin.any
+import no.skatteetaten.aurora.clerk.controller.PodItem
+import no.skatteetaten.aurora.clerk.controller.PodResourceAssembler
+import no.skatteetaten.aurora.clerk.controller.security.User
 import no.skatteetaten.aurora.clerk.service.PodService
+import no.skatteetaten.aurora.clerk.service.openshift.token.UserDetailsProvider
 import org.junit.jupiter.api.BeforeEach
 import org.mockito.BDDMockito.given
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.security.test.context.support.WithMockUser
 
 @WithMockUser
-class PodBase : ContractBase() {
+open class PodBase : ContractBase() {
 
     @MockBean
     private lateinit var podService: PodService
 
-    // TODO kan vi bruke samme metode som i controller testene her og slippe å mocke userDetailsProvider?
-    // @MockBean
+    @MockBean
+    private lateinit var assembler: PodResourceAssembler
 
-    // private lateinit var userDetailsProvider: UserDetailsProvider
+    // TODO kan vi bruke samme metode som i controller testene her og slippe å mocke userDetailsProvider?
+    @MockBean
+    private lateinit var userDetailsProvider: UserDetailsProvider
 
     @BeforeEach
     fun setUp() {
         withContractResponses(this) {
-            //       given(userDetailsProvider.getAuthenticatedUser()).willReturn(User("system:serviceaccount:jedi:default", "token"))
-            given(podService.getPodItems(any(), any())).willReturn(it.response())
+            given(userDetailsProvider.getAuthenticatedUser()).willReturn(
+                User(
+                    "system:serviceaccount:jedi-test:default",
+                    "token"
+                )
+            )
+            given(assembler.toAuroraResource<PodItem>(any(), any())).willReturn(it.response())
         }
     }
 }
