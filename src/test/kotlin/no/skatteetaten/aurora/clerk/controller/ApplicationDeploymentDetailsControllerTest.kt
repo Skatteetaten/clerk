bdc7424bbb3bb83c3aa247af70b487551f78bd35
Bjarte Stien Karlsen <bjarte.karlsen@skatteetaten.no>
2019-03-04 14:35:26 +0100

    update to latest plugin

--- src/test/kotlin/no/skatteetaten/aurora/clerk/controller/ApplicationDeploymentDetailsControllerTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/clerk/controller/ApplicationDeploymentDetailsControllerTest.kt
@@ -24,7 +24,7 @@ import java.time.Instant
     BearerAuthenticationManager::class
 )
 @AutoConfigureWebClient
-@AutoConfigureRestDocs("build/docs/generated-snippets")
+@AutoConfigureRestDocs
 class ApplicationDeploymentDetailsControllerTest : AbstractSecurityControllerTest() {
 
     @MockBean
