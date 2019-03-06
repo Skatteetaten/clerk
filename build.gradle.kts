96fb9001927e87218331ea791d756d0134ba7e2b
Bjarte Stien Karlsen <bjarte.karlsen@skatteetaten.no>
2019-03-06 13:41:14 +0100

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,33 +9,39 @@ buildscript {
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.21"
     id("org.jetbrains.kotlin.plugin.spring") version "1.3.21"
-    id("org.springframework.boot") version "2.1.3.RELEASE"
     id("org.jlleitschuh.gradle.ktlint") version "7.1.0"
+
+    id("org.springframework.boot") version "2.1.3.RELEASE"
+    id("org.asciidoctor.convert") version "1.6.0"
+
+    id("org.sonarqube") version "2.7"
+
+    id("com.gorylenko.gradle-git-properties") version "2.0.0"
     id("com.github.ben-manes.versions") version "0.21.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.9"
-    id("com.gorylenko.gradle-git-properties") version "2.0.0"
-    id("org.sonarqube") version "2.7"
-    id("org.asciidoctor.convert") version "1.6.0"
-    id("no.skatteetaten.gradle.aurora") version "1.0.0-rc2"
+
+    id("no.skatteetaten.gradle.aurora") version "1.0.0-rc3"
 }
 
 apply(plugin = "spring-cloud-contract")
 
 dependencies {
     implementation("io.fabric8:openshift-client:4.1.3")
+    testImplementation("io.fabric8:openshift-server-mock:4.1.3")
+    testImplementation("com.fkorotkov:kubernetes-dsl:2.0.1")
+    testImplementation("com.squareup.okhttp3:mockwebserver:3.12.0")
+
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
-
     testImplementation("org.springframework.cloud:spring-cloud-starter-contract-verifier")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.fabric8:openshift-server-mock:4.1.3")
+
     testImplementation("io.mockk:mockk:1.9.1")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.13")
-    testImplementation("com.fkorotkov:kubernetes-dsl:2.0.1")
     testImplementation("com.nhaarman:mockito-kotlin:1.6.0")
-    testImplementation("com.squareup.okhttp3:mockwebserver:3.12.0")
 }
 
 tasks.findByPath("compileTestGroovy")?.enabled = false
+
