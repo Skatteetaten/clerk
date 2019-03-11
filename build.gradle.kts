b72b6693dc56d44d6605bdded07b7f20a61df3aa
Bjarte Stien Karlsen <bjarte.karlsen@skatteetaten.no>
2019-03-11 14:26:27 +0100

    fix plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,6 @@ plugins {
     id("se.patrikerdes.use-latest-versions") version "0.2.9"
 
     id("no.skatteetaten.gradle.aurora") version "2.0.2"
-    id("no.skatteetaten.gradle.aurora") version "2.0.0"
 
 }
 
