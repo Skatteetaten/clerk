1885f5691373fb4f958d434056fcc7d253c36278
Bjarte S. Karlsen <bjarte@bjartek.org>
2019-03-19 09:26:11 +0100

    feature/AOS-3275 (#3)
    * upgrade dependency, remove sonar,
    
    * remove global gradle settings
    
    * update mockwebserver dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,6 @@ plugins {
     id("org.springframework.boot") version "2.1.3.RELEASE"
     id("org.asciidoctor.convert") version "1.6.0"
 
-    id("org.sonarqube") version "2.7"
-
     id("com.gorylenko.gradle-git-properties") version "2.0.0"
     id("com.github.ben-manes.versions") version "0.21.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.9"
@@ -30,14 +28,14 @@ dependencies {
     implementation("io.fabric8:openshift-client:4.1.3")
     testImplementation("io.fabric8:openshift-server-mock:4.1.3")
     testImplementation("com.fkorotkov:kubernetes-dsl:2.0.1")
-    testImplementation("com.squareup.okhttp3:mockwebserver:3.13.1")
+    testImplementation("com.squareup.okhttp3:mockwebserver:3.14.0")
 
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    testImplementation("io.mockk:mockk:1.9.1")
+    testImplementation("io.mockk:mockk:1.9.2")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.13")
     testImplementation("com.nhaarman:mockito-kotlin:1.6.0")
 }
