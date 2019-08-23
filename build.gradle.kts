7f89a1a47b22bfcbf5babea218d5c62060f6c68b
Erlend Haugsnes <Erlend.Haugsnes@skatteetaten.no>
2019-08-23 10:18:12 +0200

    gradle 5.6 Update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,42 +1,32 @@
-buildscript {
-    dependencies {
-        // must specify this in gradle.properties since the same version must be here and in aurora plugin
-        val springCloudContractVersion: String = project.property("aurora.springCloudContractVersion") as String
-        classpath("org.springframework.cloud:spring-cloud-contract-gradle-plugin:$springCloudContractVersion")
-    }
-}
-
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.21"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.3.21"
-    id("org.jlleitschuh.gradle.ktlint") version "8.0.0"
+    id("org.springframework.cloud.contract")
+    id("org.jetbrains.kotlin.jvm") version "1.3.41"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.41"
+    id("org.jlleitschuh.gradle.ktlint") version "8.2.0"
     id("org.sonarqube") version "2.7.1"
 
     id("org.springframework.boot") version "2.1.7.RELEASE"
     id("org.asciidoctor.convert") version "1.6.0"
 
     id("com.gorylenko.gradle-git-properties") version "2.0.0"
-    id("com.github.ben-manes.versions") version "0.21.0"
-    id("se.patrikerdes.use-latest-versions") version "0.2.9"
+    id("com.github.ben-manes.versions") version "0.22.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.12"
 
-    id("no.skatteetaten.gradle.aurora") version "2.4.0"
+    id("no.skatteetaten.gradle.aurora") version "2.4.2"
 }
 
-apply(plugin = "spring-cloud-contract")
-
 dependencies {
-    implementation("io.fabric8:openshift-client:4.1.3")
-    testImplementation("io.fabric8:openshift-server-mock:4.1.3")
-    testImplementation("com.fkorotkov:kubernetes-dsl:2.0.1")
-    testImplementation("com.squareup.okhttp3:mockwebserver:3.14.0")
+    implementation("io.fabric8:openshift-client:4.4.1")
+    testImplementation("io.fabric8:openshift-server-mock:4.4.1")
+    testImplementation("com.fkorotkov:kubernetes-dsl:2.1")
 
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    testImplementation("io.mockk:mockk:1.9.2")
-    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.13")
+    testImplementation("io.mockk:mockk:1.9.3")
+    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.19")
     testImplementation("com.nhaarman:mockito-kotlin:1.6.0")
-    testImplementation("no.skatteetaten.aurora:mockmvc-extensions-kotlin:0.6.2")
+    testImplementation("no.skatteetaten.aurora:mockmvc-extensions-kotlin:1.0.0")
 }
