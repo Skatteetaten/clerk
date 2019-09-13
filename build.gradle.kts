80b1d5dd46b60904dc6f3992aa273a977088243e
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2019-09-13 15:13:31 +0200

    iq, bump dependencies, gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,24 @@
 plugins {
     id("org.springframework.cloud.contract")
-    id("org.jetbrains.kotlin.jvm") version "1.3.41"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.3.41"
+    id("org.jetbrains.kotlin.jvm") version "1.3.50"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.50"
     id("org.jlleitschuh.gradle.ktlint") version "8.2.0"
     id("org.sonarqube") version "2.7.1"
 
-    id("org.springframework.boot") version "2.1.7.RELEASE"
+    id("org.springframework.boot") version "2.1.8.RELEASE"
     id("org.asciidoctor.convert") version "2.3.0"
 
-    id("com.gorylenko.gradle-git-properties") version "2.0.0"
-    id("com.github.ben-manes.versions") version "0.22.0"
+    id("com.gorylenko.gradle-git-properties") version "2.1.0"
+    id("com.github.ben-manes.versions") version "0.25.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.12"
 
     id("no.skatteetaten.gradle.aurora") version "2.4.2"
 }
 
 dependencies {
-    implementation("io.fabric8:openshift-client:4.4.1")
-    testImplementation("io.fabric8:openshift-server-mock:4.4.1")
-    testImplementation("com.fkorotkov:kubernetes-dsl:2.1")
+    implementation("io.fabric8:openshift-client:4.5.1")
+    testImplementation("io.fabric8:openshift-server-mock:4.5.1")
+    testImplementation("com.fkorotkov:kubernetes-dsl:2.2")
 
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
